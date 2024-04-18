faaeb60727198b61afea4b8385ba44fb4bdb5313
YeslieSnayder <kuzmickiy2000@mail.ru>
2024-04-19 02:52:48 +0300

    feat: complete auth controller

--- backend/internals/auth/token.py
+++ backend/internals/auth/token.py
@@ -2,4 +2,8 @@ from backend.models import User
 
 
 def decode_token(token: str) -> User:
-    return User(username=token)
+    return User(username=token, password='booba')
+
+
+def create_token(user: User) -> str:
+    return 'aboba'
