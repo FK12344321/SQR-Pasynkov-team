4e08f8753d2f0aebb2dc761b2fc5f2d9881b1fb3
avkuzmitsk <avkuzmitsk@mts.ru>
2024-04-27 13:58:39 +0300

    feat: use jwt tokens

--- backend/app/internals/auth/token.py
+++ backend/app/internals/auth/token.py
@@ -1,4 +1,4 @@
-import base64
+import jwt
 
 from app.models import User, UserCredentials, IncorrectToken
 from app.database.users import get_user_by_username
@@ -6,18 +6,20 @@ from app.database.users import get_user_by_username
 
 def decode_token(token: str) -> User:
     """Decodes a Bearer token. Can raise IncorrectToken exception."""
-    try:
-        token_data = base64.b64decode(token.encode('utf-8')).decode('utf-8')
-        username, reversed_username = token_data.split(sep=' ', maxsplit=1)
-        if username != reversed_username[::-1]:
-            raise IncorrectToken('Bearer')
-        return get_user_by_username(username)
-    except Exception:
-        raise IncorrectToken('Bearer')
+    header_data = jwt.get_unverified_header(token)
+    payload_data = jwt.decode(token, key='my_super_secret', algorithms=[header_data['alg', ]])
+    return get_user_by_username(payload_data['username'])
 
 
 def generate_token_from_user(user: User) -> UserCredentials:
-    token = f"{user.username} {user.username[::-1]}"
-    token_bytes = token.encode('utf-8')
-    base64_token = base64.b64encode(token_bytes).decode('utf-8')
-    return UserCredentials(access_token=base64_token, refresh_token=base64_token)
+    payload_data = {
+        "username": user.username,
+    }
+
+    token = jwt.encode(
+        payload=payload_data,
+        key='my_super_secret',
+        algorithm='HS256',
+    )
+
+    return UserCredentials(access_token=token, refresh_token=token)
