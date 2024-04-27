9e2e09b04037e40304d4d286de5836c43cbd51c0
YeslieSnayder <kuzmickiy2000@mail.ru>
2024-04-27 20:48:13 +0300

    feat: db name to config

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     auth_token_secret: str = Field("xxx", alias="TOKEN_SECRET")
     auth_token_alg: str = "HS256"
+    db_file_path: str = "/pasynkov.db"
 
     model_config = SettingsConfigDict(env_file='.env', extra=None)
 
